c407c8bc99d66a586f67d9ec992b4f8c64f66f01
Elixeus <eliseowang@gmail.com>
2016-02-17 16:03:38 -0500

    use time delta to create a new file every hour

--- scraper.py
+++ scraper.py
@@ -16,38 +16,38 @@ Start working on the one hour zipper,
 TODO: familiarize with CRON, which is very good at expection handling
 '''
 
-# if __name__ == '__main__':
-try:
-	while True:
-			url = ('http://api.prod.obanyc.com/api/siri/'
+# record the start time stamp
+sts_init = dt.datetime.now()
+url = ('http://api.prod.obanyc.com/api/siri/'
 				   'vehicle-monitoring.json')
-			params = {'key':sys.argv[1], # input api key as an sys argv
-		  			  'version':1, # choose the version of the api, choose 2 here
-		  			  'VehicleMonitoringDetailLevel': ''} # nothing here
-			# use the requests module to pull down the data
-			response = requests.get(url, params)
-			# check if the url works
-			response.raise_for_status()
-			# print response.content
-			# retrieve the json file
-			data_raw = response.json()
-			# name the file with the access time
-			filename = 'data_%s.json' %(
-				dt.datetime.now().strftime('%y_%m_%d_%H_%M_%S'))
-			while True:
-				# load the old json file
-				with open(filename, 'r') as fr:
-					jload = json.load(fr)
-				# append the new data to the old json file
-				jload.update(data_raw)
-				# write the new content into the json file
-				with open(filename, 'w') as fw:
-					jwrite = json.dump(data_raw, fw)
-				# rest for 30 seconds
-				time.sleep(30)
-				# notify the new start
-				print 'Start again'
-			time.sleep(60*60)
+params = {'key':sys.argv[1], # input api key as an sys argv
+			  'version':1, # choose the version of the api, choose 2 here
+			  'VehicleMonitoringDetailLevel': ''} # nothing here
 
-except KeyboardInterrupt:
-	print 'interrupted!'
+while True:
+	try:
+		sts = dt.datetime.now()
+		# use the requests module to pull down the data
+		response = requests.get(url, params)
+		# check if the url works
+		response.raise_for_status()
+		# retrieve the json file
+		data_raw = response.json()
+		# name the file with the access time
+		filename = 'data_%s.json' %(sts_init.strftime('%y_%m_%d_%H_%M_%S'))
+		# load the old json file
+		with open(filename, 'r') as fr:
+			jload = json.load(fr)
+		# append the new data to the old json file
+		jload.update(data_raw)
+		# write the new content into the json file
+		with open(filename, 'w') as fw:
+			jwrite = json.dump(data_raw, fw)
+		# compare the time delta
+		time.sleep(30)
+		# update the sts if the time delta is larger than 1 hour
+		if (sts - sts_init) >= dt.time(1,0,0):
+			sts_init = sts
+
+	except KeyboardInterrupt:
+		print 'interrupted!'
