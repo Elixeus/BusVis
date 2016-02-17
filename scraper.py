6cfff2d57d94c0109c369677eb6eb5f8bafa2d7a
Elixeus <eliseowang@gmail.com>
2016-02-17 14:57:47 -0500

    append content to the same file every 30 seconds

--- scraper.py
+++ scraper.py
@@ -12,13 +12,15 @@ sys.argv[1]: the api key of the user.
 '''
 
 '''
-Start working on the one hour zipper
+Start working on the one hour zipper,
+TODO: familiarize with CRON, which is very good at expection handling
 '''
 
 # if __name__ == '__main__':
 try:
 	while True:
-			url = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json'
+			url = ('http://api.prod.obanyc.com/api/siri/'
+				   'vehicle-monitoring.json')
 			params = {'key':sys.argv[1], # input api key as an sys argv
 		  			  'version':1, # choose the version of the api, choose 2 here
 		  			  'VehicleMonitoringDetailLevel': ''} # nothing here
@@ -26,18 +28,22 @@ try:
 			response = requests.get(url, params)
 			# check if the url works
 			response.raise_for_status()
-
-			print response.content
+			# print response.content
 			# retrieve the json file
 			data_raw = response.json()
 			# nanme the file with the access time
 			filename = 'data_%s.json' %(
 				dt.datetime.now().strftime('%y_%m_%d_%H_%M_%S'))
-			# save the data to a json file
-			with open(filename, 'w') as fh:
-				json.dump(data_raw, fh)
-			# rest for 3 minutes
-			time.sleep(5*60)
+			# load the old json file
+			with open(filename, 'r') as fr:
+				jload = json.load(fr)
+			# append the new data to the old json file
+			jload.update(data_raw)
+			# write the new content into the json file
+			with open(filename, 'w') as fw:
+				jwrite = json.dump(data_raw, fw)
+			# rest for 30 seconds
+			time.sleep(30)
 			# notify the new start
 			print 'Start again'
 
