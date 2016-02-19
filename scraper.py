02aec3219b2258b61eedc4d07ff27481507913a9
Elixeus <eliseowang@gmail.com>
2016-02-18 23:08:59 -0500

    change mode from r and w to a

--- scraper.py
+++ scraper.py
@@ -12,8 +12,9 @@ sys.argv[1]: the api key of the user.
 '''
 
 '''
-Start working on the one hour zipper,
-TODO: familiarize with CRON, which is very good at expection handling
+pull down the data every 30 seconds,
+append the data to the existing file,
+print an empty line,
 '''
 if __name__ == '__main__':
 	# record the start time stamp
@@ -31,26 +32,21 @@ if __name__ == '__main__':
 			# check if the url works
 			response.raise_for_status()
 			# retrieve the json file
-			data_raw = response.json()
-			# name the file with the access time
-			filename = 'data_%s.json' %(sts_init.strftime('%y_%m_%d_%H_%M_%S'))
 			try:
-				# load the old json file
-				with open(filename, 'r') as fr:
-					jload = json.load(fr)
-					# append the new data to the old json file
-					jload.update(data_raw)
-				# write the new content into the json file
-				with open(filename, 'w') as fw:
-					json.dump(data_raw, fw)
-			except IOError:
-				with open(filename, 'w') as fw:
-					jwrite = json.dump(data_raw, fw)
-			finally:
-				time.sleep(30)
-				sts = dt.datetime.now()
-				# update the sts if the time delta is larger than 1 hour
-				if (sts - sts_init) >= dt.timedelta(seconds = 3600):
-					sts_init = sts
+				data_raw = response.json()
+			# name the file with the access time
+			filename = 'BusData_%s.json' %(sts_init.strftime(
+						'%y_%m_%d_%H_%M_%S'))
+			# deal
+			fw = open(filename, 'a')
+			json.dump(data_raw, fw)
+			json.dump('\n', fw)
+			fw.close()
+			print 'data dumped'
+			time.sleep(30)
+			sts = dt.datetime.now()
+			# update the sts if the time delta is larger than 1 hour
+			if (sts - sts_init) >= dt.timedelta(seconds = 3600):
+				sts_init = sts
 	except KeyboardInterrupt:
 		print 'interrupted!'
