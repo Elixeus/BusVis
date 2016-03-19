a18a8a8acdfaa4ad018731d97840175dc150aef7
Elixeus <eliseowang@gmail.com>
2016-03-19 12:12:41 -0400

    now can manually exit the program

--- main.py
+++ main.py
@@ -12,13 +12,17 @@ params = {'key': sys.argv[1],  # input api key as an sys argv
           'VehicleMonitoringDetailLevel': ''}  # nothing here
 
 if __name__ == '__main__':
-    while True:
-        ts = dt.datetime.now()
-        filename = 'MTABusData_%s.txt' % (ts.strftime(
-            '%y_%m_%d_%H_%M_%S'))
-        Scraper(url, params, filename)  # where scraping happens
-        time.sleep(30)
-        ts_one = dt.datetime.now()
-        if (ts_one - ts) >= dt.timedelta(s=3600):
-            Zipper(filename)  # dump the data into a local file and zip it
-            ts = ts_one
+    try:
+        while True:
+            ts = dt.datetime.now()
+            filename = 'MTABusData_%s.txt' % (ts.strftime(
+                '%y_%m_%d_%H_%M_%S'))
+            Scraper(url, params, filename)  # where scraping happens
+            time.sleep(30)
+            ts_one = dt.datetime.now()
+            if (ts_one - ts) >= dt.timedelta(s=3600):
+                Zipper(filename)  # dump the data into a local file and zip it
+                ts = ts_one
+    except KeyboardInterrupt:
+        print 'Process Interrupted!'
+        sys.exit()
