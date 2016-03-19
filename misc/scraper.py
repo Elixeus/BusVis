0b012d29c4bd615dffbcaab7a8d0308af96f3594
Elixeus <eliseowang@gmail.com>
2016-03-19 12:30:38 -0400

    add comment and TODO list to the scraper function

--- misc/scraper.py
+++ misc/scraper.py
@@ -4,7 +4,17 @@ import time
 
 
 def Scraper(url, params, filename):
+    '''
+    Scraper is a generic function that can be used to scrape data from any API.
+    url: the url of the API.
+    params: the parameters required by the API e.g. API KEY, consumer secret...
+    filename: create a file on the harddrive where the data can be stored.
+    '''
     fw = open(filename, 'a')
+    '''
+    there are 3 major HTTP request failures: Client, Server, and Caller.
+    TODO: 1. study these failures more carefully 2. tweak the timeout range
+    '''
     try:
         response = requests.get(url, params, timeout=(0.001, 10))
     except requests.exceptions.ConnectionError as e:  # connection error
